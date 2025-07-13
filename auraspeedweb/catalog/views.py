3d69bcdc4d52a1830b1edefd1b6cf4fbd284e3ab
luisma <mrluisgamerps3@gmail.com>
2025-07-13 16:36:06 -0400

    first commit

---
+++ auraspeedweb/catalog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Category, Product
+
+def catalog(request):
+    categories = Category.objects.all()
+    products_by_category = {}
+    
+    for category in categories:
+        products_by_category[category] = Product.objects.filter(category=category)
+    
+    context = {
+        'categories': categories,
+        'products_by_category': products_by_category
+    }
+    return render(request, 'catalog.html', context)
