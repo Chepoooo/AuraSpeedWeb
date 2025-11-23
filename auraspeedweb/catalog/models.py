1e5b99aebdb93c589734750465bd6efc72397969
luisma <mrluisgamerps3@gmail.com>
2025-11-23 14:32:26 -0400

    segundo commit antes de crear la 2da rama para proceder

--- auraspeedweb/catalog/models.py
+++ auraspeedweb/catalog/models.py
@@ -11,10 +11,11 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()
+    stock = models.IntegerField(default=0)
     image = models.ImageField(upload_to='products/')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
     
     def __str__(self):
         return self.name
